b7d95c315a3c16ff70453c82c7c6b776ddf791fd
rboyko <rostyslav.boyko@masterofcode.com>
2018-04-11 16:41:06 +0300

    create server

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-var http = require('http')
+var express = require('express');
+var mime_type = require('./mime_type')
+
 var port = process.env.BOT_PORT || 3000;
 
-http.createServer(function(req,resp){
-    resp.writeHead(200,{'Content-Type': 'text/plain'});
-    resp.end('Hello world!');
-}).listen(port);
-console.log("Bot started on http://127.0.0.1:"+port);
+var app = express();
+
+app.set('port',port)
+
+app.get('/',function(req,res){
+   res.type(mime_type.APPLICATION_TYPE_TEXT);
+   res.send('Hello world !!!')
+});
+
+app.use(function(req,res){
+    res.type(mime_type.APPLICATION_TYPE_TEXT);
+    res.status(404);
+    res.send('404 - Not found')
+});
+
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.type(mime_type.APPLICATION_TYPE_TEXT);
+    res.status(500);
+    res.send('500 - Server error');
+});
+
+app.listen(app.get('port'),function(){
+    console.log('Server run on http://127.0.0.1:'+port);
+});
